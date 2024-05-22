56f060c0adb90678d736d620485e51593a9af50a
SungJung <sungung0616@gmail.com>
2024-05-23 00:45:55 +0900

    todo-be v1

---
+++ README.md
@@ -0,0 +1,17 @@
+# TODO-BE
+
+This is a backend application for a TODO application built with Node.js and Express.
+
+## Features
+
+- Create tasks
+- Retrieve tasks
+- Update tasks
+- Delete tasks
+
+## Technologies
+
+- Node.js
+- Express
+- MongoDB
+- Mongoose
---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/api',indexRouter);
+
+const mongoURI = `mongodb://localhost:27017/todo-demo`;
+
+mongoose.connect(mongoURI,{ useNewUrlParser : true})
+.then(()=>{
+    console.log("mongoose connected");
+})
+.catch((err)=>{
+    console.error("Mongoose connection error:", err);
+})
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
